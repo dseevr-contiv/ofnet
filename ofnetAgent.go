77beb9d5265a679a64de2bec37b84107b6d140d2
Divya Vavili <vavili.divya@gmail.com>
2016-10-06 10:31:43 -0700

    Changing ofnet signature

--- ofnetAgent.go
+++ ofnetAgent.go
@@ -125,11 +125,9 @@ const (
 )
 
 // Create a new Ofnet agent and initialize it
-/*  routerInfo[0] -> Uplink nexthop interface
- */
 func NewOfnetAgent(bridgeName string, dpName string, localIp net.IP, rpcPort uint16,
-	ovsPort uint16, routerInfo ...string) (*OfnetAgent, error) {
-	log.Infof("Creating new ofnet agent for %s,%s,%d,%d,%d,%v \n", bridgeName, dpName, localIp, rpcPort, ovsPort, routerInfo)
+	ovsPort uint16, vlanIntf []string) (*OfnetAgent, error) {
+	log.Infof("Creating new ofnet agent for %s,%s,%d,%d,%d,%v \n", bridgeName, dpName, localIp, rpcPort, ovsPort, vlanIntf)
 	agent := new(OfnetAgent)
 
 	// Init params
@@ -188,7 +186,7 @@ func NewOfnetAgent(bridgeName string, dpName string, localIp net.IP, rpcPort uin
 		agent.datapath = NewVlrouter(agent, rpcServ)
 		agent.fwdMode = "routing"
 		agent.ovsDriver = ovsdbDriver.NewOvsDriver(bridgeName)
-		agent.protopath = NewOfnetBgp(agent, routerInfo)
+		agent.protopath = NewOfnetBgp(agent, vlanIntf)
 	default:
 		log.Fatalf("Unknown Datapath %s", dpName)
 	}
